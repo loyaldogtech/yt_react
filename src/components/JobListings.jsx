dcdb0c829a3853e1f1087949df9bbf2ec0fdd9ad
Shad Spreiter <loyaldogtech@gmail.com>
2024-06-20 17:22:27 -0700

    completed job page and read more button to backend

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -20,7 +20,7 @@ const JobListings = ({ isHome = false }) => {
             }
         };
         fetchJobs();
-    }, [isHome]);
+    }, []);
 
     return (
         <section className="bg-blue-50 px-4 py-10">
