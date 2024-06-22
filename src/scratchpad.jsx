5ddab0251192e73a93a35b3ad7b9bb9115ed3cc1
Shad Spreiter <loyaldogtech@gmail.com>
2024-06-21 22:33:50 -0700

    completed Add Job page

--- src/scratchpad.jsx
+++ src/scratchpad.jsx
@@ -1,93 +1,156 @@
-// <!-- Go Back -->
-<>
-    <section>
-        <div className="container m-auto py-6 px-6">
-            <a
-                href="/jobs.html"
-                className="text-indigo-500 hover:text-indigo-600 flex items-center"
-            >
-                <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
-            </a>
-        </div>
-    </section>
+<section className="bg-indigo-50">
+    <div className="container m-auto max-w-2xl py-24">
+        <div
+            className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
+        >
+            <form>
+                <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
-    <section className="bg-indigo-50">
-        <div className="container m-auto py-10 px-6">
-            <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                <main>
-                    <div
-                        className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
+                <div className="mb-4">
+                    <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
+                    >Job Type</label>
+                    <select
+                        id="type"
+                        name="type"
+                        className="border rounded w-full py-2 px-3"
+                        required
                     >
-                        <div className="text-gray-500 mb-4">Full-Time</div>
-                        <h1 className="text-3xl font-bold mb-4">
-                            Senior React Developer
-                        </h1>
-                        <div
-                            className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-                        >
-                            <i
-                                className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                            ></i>
-                            <p className="text-orange-700">Boston, MA</p>
-                        </div>
-                    </div>
+                        <option value="Full-Time">Full-Time</option>
+                        <option value="Part-Time">Part-Time</option>
+                        <option value="Remote">Remote</option>
+                        <option value="Internship">Internship</option>
+                    </select>
+                </div>
 
-                    <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                        <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                            Job Description
-                        </h3>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2"
+                    >Job Listing Name</label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        className="border rounded w-full py-2 px-3 mb-2"
+                        placeholder="eg. Beautiful Apartment In Miami"
+                        required
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        htmlFor="description"
+                        className="block text-gray-700 font-bold mb-2"
+                    >Description</label>
+                    <textarea
+                        id="description"
+                        name="description"
+                        className="border rounded w-full py-2 px-3"
+                        rows="4"
+                        placeholder="Add any job duties, expectations, requirements, etc"
+                    ></textarea>
+                </div>
 
-                        <p className="mb-4">
-                            We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
-                        </p>
+                <div className="mb-4">
+                    <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
+                    >Salary</label
+                    >
+                    <select
+                        id="salary"
+                        name="salary"
+                        className="border rounded w-full py-2 px-3"
+                        required
+                    >
+                        <option value="Under $50K">Under $50K</option>
+                        <option value="$50K - 60K">$50K - $60K</option>
+                        <option value="$60K - 70K">$60K - $70K</option>
+                        <option value="$70K - 80K">$70K - $80K</option>
+                        <option value="$80K - 90K">$80K - $90K</option>
+                        <option value="$90K - 100K">$90K - $100K</option>
+                        <option value="$100K - 125K">$100K - $125K</option>
+                        <option value="$125K - 150K">$125K - $150K</option>
+                        <option value="$150K - 175K">$150K - $175K</option>
+                        <option value="$175K - 200K">$175K - $200K</option>
+                        <option value="Over $200K">Over $200K</option>
+                    </select>
+                </div>
 
-                        <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 font-bold mb-2'>
+                        Location
+                    </label>
+                    <input
+                        type='text'
+                        id='location'
+                        name='location'
+                        className='border rounded w-full py-2 px-3 mb-2'
+                        placeholder='Company Location'
+                        required
+                    />
+                </div>
 
-                        <p className="mb-4">$70k - $80K / Year</p>
-                    </div>
-                </main>
+                <h3 className="text-2xl mb-5">Company Info</h3>
 
-                {/* <!-- Sidebar --> */}
-                <aside>
-                    {/* <!-- Company Info --> */}
-                    <div className="bg-white p-6 rounded-lg shadow-md">
-                        <h3 className="text-xl font-bold mb-6">Company Info</h3>
+                <div className="mb-4">
+                    <label htmlFor="company" className="block text-gray-700 font-bold mb-2"
+                    >Company Name</label>
+                    <input
+                        type="text"
+                        id="company"
+                        name="company"
+                        className="border rounded w-full py-2 px-3"
+                        placeholder="Company Name"
+                    />
+                </div>
 
-                        <h2 className="text-2xl">NewTek Solutions</h2>
+                <div className="mb-4">
+                    <label
+                        htmlFor="company_description"
+                        className="block text-gray-700 font-bold mb-2">
+                        Company Description</label>
+                    <textarea
+                        id="company_description"
+                        name="company_description"
+                        className="border rounded w-full py-2 px-3"
+                        rows="4"
+                        placeholder="What does your company do?"
+                    ></textarea>
+                </div>
 
-                        <p className="my-2">
-                            NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
-                        </p>
+                <div className="mb-4">
+                    <label
+                        htmlFor="contact_email"
+                        className="block text-gray-700 font-bold mb-2">
+                        Contact Email</label>
+                    <input
+                        type="email"
+                        id="contact_email"
+                        name="contact_email"
+                        className="border rounded w-full py-2 px-3"
+                        placeholder="Email address for applicants"
+                        required
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        htmlFor="contact_phone"
+                        className="block text-gray-700 font-bold mb-2">
+                        Contact Phone</label>
+                    <input
+                        type="tel"
+                        id="contact_phone"
+                        name="contact_phone"
+                        className="border rounded w-full py-2 px-3"
+                        placeholder="Optional phone for applicants"
+                    />
+                </div>
 
-                        <hr className="my-4" />
-
-                        <h3 className="text-xl">Contact Email:</h3>
-
-                        <p className="my-2 bg-indigo-100 p-2 font-bold">
-                            contact@newteksolutions.com
-                        </p>
-
-                        <h3 className="text-xl">Contact Phone:</h3>
-
-                        <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
-                    </div>
-
-                    {/* <!-- Manage --> */}
-                    <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                        <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                        <a
-                            href="/add-job.html"
-                            className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                        >Edit Job</a
-                        >
-                        <button
-                            className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                        >
-                            Delete Job
-                        </button>
-                    </div>
-                </aside>
-            </div>
+                <div>
+                    <button
+                        className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Add Job
+                    </button>
+                </div>
+            </form>
         </div>
-    </section>
-</>
+    </div>
+</section>;
